a59668f07225229e610f44d7a40362ab7cda34e7
Arshdeep <aschhoka@sfu.ca>
2021-03-25 23:31:07 -0700

    parse flags entered as single argument

--- myls.c
+++ myls.c
@@ -21,6 +21,22 @@ int main(int argc,char **argv)
     bool i_flag = 0;
     bool l_flag = 0;
 
+    int i;
+    if(argv[1]){ 
+        for(i = 0; i < strlen(argv[1]); i++) {
+            if(argv[1][i] == 'R'){
+                r_flag = 1;
+                printf("R flag was entered\n");
+            } else if(argv[1][i] == 'i') {
+                i_flag = 1;
+                printf("i flag was entered\n");
+            } else if(argv[1][i] == 'l') {
+                l_flag = 1;
+                printf("l flag was entered\n");
+            }
+        }
+    }
+    
     listDir(r_flag, i_flag, l_flag);
     return 0; 
 } 
