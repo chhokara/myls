1c5bf74d5bd0fc25d2b2d0fbb4cdb82687eebb0e
Arshdeep <aschhoka@sfu.ca>
2021-03-28 18:38:02 -0700

    error checking for non existent directories and invalid flags

--- myls.c
+++ myls.c
@@ -3,8 +3,10 @@
 #include <stdlib.h> 
 #include <dirent.h> 
 #include <stdbool.h>
+#include <errno.h>
 
-void listDir(bool r_flag, bool i_flag, bool l_flag) {
+void listDir(bool r_flag, bool i_flag, bool l_flag, char * directory) {
+    // these are test outputs to see if flags are being parsed correctly
     if(r_flag) {
         printf("R flag was entered\n");
     } 
@@ -16,12 +18,31 @@ void listDir(bool r_flag, bool i_flag, bool l_flag) {
     }
     DIR *d;
     struct dirent * dir;
-    d = opendir("./");
-    while((dir = readdir(d))) {
-        printf("%s\n", dir->d_name);
+    // this is the case when user specifies no directory
+    if(strcmp(directory, "./myls") == 0) {
+        d = opendir("./");
+        while((dir = readdir(d))) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    } 
+    // this is the case when the user specifies a directory
+    else {
+        d = opendir(directory);
+        // directory exists
+        if(d) {
+            while((dir = readdir(d))) {
+                printf("%s\n", dir->d_name);
+            }
+            closedir(d);
+        }
+        // directory does not exist
+        else if (ENOENT == errno) {
+            printf("Error: directory does not exist\n");
+            exit(1);
+        }
+        
     }
-    closedir(d);
-    
 }
 
 int main(int argc,char **argv) 
@@ -29,61 +50,37 @@ int main(int argc,char **argv)
     bool r_flag = 0;
     bool i_flag = 0;
     bool l_flag = 0;
+    char dir[1024] = "";
 
     int i;
-    if(argv[1] && argv[1][0] == '-'){ 
-        for(i = 1; i < strlen(argv[1]); i++) {
-            if(argv[1][i] == 'R'){
-                r_flag = 1;
+    int j;
+    // parse user input for flags and directories
+    for(i = 0; i < argc; i++) {
+        for(j = 1; argv[i][j] != '\0'; j++) {
+            // any input beginning with '-' is treated as a potential flag
+            if(argv[i][0] == '-'){
+                if(argv[i][j] == 'R') {
+                    r_flag = 1;
+                }
+                if(argv[i][j] == 'i') {
+                    i_flag = 1;
+                }
+                if(argv[i][j] == 'l') {
+                    l_flag = 1;
+                }
+                // if user enters a flag that is neither R, i, or l, we print error message and exit
+                if(argv[i][j] != 'R' && argv[i][j] != 'i' && argv[i][j] != 'l') {
+                    printf("Error: invalid flag\n");
+                    exit(1);
+                }
             } 
-            if(argv[1][i] == 'i') {
-                i_flag = 1;
-            } 
-            if(argv[1][i] == 'l') {
-                l_flag = 1;
-            }
-            if(argv[1][i] != 'R' && argv[1][i] != 'i' && argv[1][i] != 'l') {
-                printf("Error: invalid flag entered\n");
-                exit(1);
-            }
-        }
-    } 
-    if(argv[2] && argv[2][0] == '-') {
-        for(i = 1; i < strlen(argv[2]); i++) {
-            if(argv[2][i] == 'R'){
-                r_flag = 1;
-            } 
-            if(argv[2][i] == 'i') {
-                i_flag = 1;
-            } 
-            if(argv[2][i] == 'l') {
-                l_flag = 1;
-            }
-            if(argv[2][i] != 'R' && argv[2][i] != 'i' && argv[2][i] != 'l') {
-                printf("Error: invalid flag entered\n");
-                exit(1);
-            }
-        }
-    } 
-    if(argv[3] && argv[3][0] == '-') {
-        for(i = 1; i < strlen(argv[3]); i++) {
-            if(argv[3][i] == 'R'){
-                r_flag = 1;
-            } 
-            if(argv[3][i] == 'i') {
-                i_flag = 1;
-            } 
-            if(argv[3][i] == 'l') {
-                l_flag = 1;
-            }
-            if(argv[3][i] != 'R' && argv[3][i] != 'i' && argv[3][i] != 'l') {
-                printf("Error: invalid flag entered\n");
-                exit(1);
+            // if input does not begin with a '-' we treat it as a directory that the user is specifying
+            else {
+                strcpy(dir, argv[i]);
             }
         }
     }
-    
-    listDir(r_flag, i_flag, l_flag);
+    listDir(r_flag, i_flag, l_flag, dir);
     return 0; 
 } 
 
